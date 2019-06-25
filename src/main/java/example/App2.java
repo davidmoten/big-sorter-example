ad94514109a9117cebe93f84d9ce913b8768c172
Dave Moten <davidmoten@gmail.com>
2019-06-26 06:47:13 +1000

    count duplicate lines in App2.java

--- src/main/java/example/App2.java
+++ src/main/java/example/App2.java
@@ -1,22 +1,45 @@
 package example;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Comparator;
 
 import com.github.davidmoten.bigsorter.LineDelimiter;
+import com.github.davidmoten.bigsorter.Reader;
 import com.github.davidmoten.bigsorter.Serializer;
 import com.github.davidmoten.bigsorter.Sorter;
 
 public class App2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        Serializer<String> serializer = Serializer.linesUtf8(LineDelimiter.LINE_FEED);
+        File output = new File("target/data2.sorted.txt");
         Sorter //
-                .serializer(Serializer.linesUtf8(LineDelimiter.LINE_FEED)) //
+                .serializer(serializer) //
                 .comparator(Comparator.naturalOrder()) //
                 .input(new File("src/test/resources/data2.txt")) //
-                .output(new File("target/data2.sorted.txt")) //
+                .output(output) //
                 .loggerStdOut() //
                 .sort();
+
+        // now count duplicates in output
+        try (InputStream in = new BufferedInputStream(new FileInputStream(output));
+                Reader<String> r = serializer.createReader(in)) {
+            long dups = 0;
+            String last = null;
+            String string;
+            while ((string = r.read()) != null) {
+                if (last != null && last.equals(string)) {
+                    dups++;
+                }
+                last = string;
+            }
+            System.out.println("duplicateLines=" + dups);
+        }
     }
 
 }
